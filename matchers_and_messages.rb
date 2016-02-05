7a408e2edbc457533283d7d12ed6fd819a96f278
User Ec2-user <ec2-user@ip-172-31-12-158.us-west-2.compute.internal>
2016-02-05 03:09:31 +0000

    YEAH it works ... kinda

--- matchers_and_messages.rb
+++ matchers_and_messages.rb
@@ -1,16 +1,20 @@
-def come_here?
+def come_here?(conversation)
   proc do |event|
     text = event.message.text
     (text.downcase =~ /come.*(in|here)/) ||
-    (text.downcase =~ /join/) ||
-    (text.downcase =~ /get.*here/)
+    (text.downcase =~ /join/)            ||
+    (text.downcase =~ /get.*here/)       ||
+    (
+      conversation == :asked_if_i_should_join_voice &&
+      text.downcase =~ /yes|yeah|sure|alright|okay|yes|do\s+it|pl[sz]/
+    )
   end
 end
 
 def im_here
   [
     "Here", "Hi", "I'm here", "'Kay", "Alright", "I'm in",
-    "Made it"
+    "'Kay", "Cool", "Here I am", "Okay"
   ]
     .sample
 end
@@ -32,4 +36,31 @@ def cant_do_voice
     "No point in me being in this voice channel", "Whatever",
     "Tell @Resonious to fix my audio"
   ]
+    .sample
+end
+
+def play_url?
+  proc do |event|
+    text = event.message.text
+    text =~ /https?:\/\//
+  end
+end
+
+def im_not_in_a_voice_channel(user)
+  [
+    "#{user.mention} Would you like me to join your voice channel?",
+    "#{user.mention} I'm not in a voice channel, should I join yours?",
+    "Should I join your voice channel #{user.mention}?",
+    "#{user.mention} want me to join your voice channel?"
+  ]
+    .sample
+end
+
+def missed_play(user)
+  [
+    "#{user.mention} sorry, missed that",
+    "#{user.mention} if your url has whitespace in it, I probably can't read it. Sorry",
+    "#{user.mention} couldn't read your url for some reason"
+  ]
+    .sample
 end
--- resmus.rb
+++ resmus.rb
@@ -18,19 +18,71 @@ end
 
 bot = Discordrb::Bot.new(Config.email, Config.password)
 
+def remember(user, key, value)
+  @memory[user.id] ||= {}
+  @memory[user.id][key] = value
+end
+
+def recall(user, key)
+  @memory[user.id].try(:[], key)
+end
+
+def download_and_play_url(bot, channel_id, url)
+  proc do
+    unless /youtu\.?be/ =~ url
+      bot.send_message(channel_id, "Hey wait, that's not a youtube URL..")
+      next
+    end
+
+    unless /\/watch\?v=(?<filename>[\w-]+)/ =~ url
+      bot.send_message(channel_id, "Ah, sorry, couldn't parse the URL")
+      next
+    end
+
+    filename += ".ogg"
+    filename = "download/#{filename}"
+    if File.exists?(filename)
+      bot.send_message(channel_id, "And we're playing!")
+      bot.voice.play_file(filename)
+      next
+    else
+      bot.send_message(channel_id, "One set, gotta download this")
+      command = %(youtube-dl -o "#{filename.gsub(/\.mp3$/, '.%(ext)s')}" --extract-audio #{url})
+      puts command
+      system(command)
+      bot.send_message(channel_id, "Done!")
+    end
+
+    if File.exists?(filename)
+      bot.send_message(channel_id, "Now we're playing!")
+      bot.voice.play_file(filename)
+    else
+      bot.send_message(channel_id, "Actually I fucked it up. Sorry.")
+    end
+  end
+end
+
 bot.message do |event|
   next unless event.message.mentions.blank?
 end
 
+@conversation = {}
+@memory = {}
 bot.mention do |event|
   @last_channel = event.channel
+  user = bot.user(event.author.id)
 
+  puts "Conversation's at #{@conversation[user.id].inspect}"
   case event
-  when come_here?
-    user = bot.user(event.author.id)
+  when come_here?(@conversation[user.id])
     if voice_channel = user.voice_channel
       begin
         bot.voice_connect(voice_channel)
+
+        if @conversation[user.id] == :asked_if_i_should_join_voice
+          @conversation[user.id] = nil
+          Thread.new(&download_and_play_url(bot, event.channel.id, recall(user, :play_url)))
+        end
       rescue StandardError => e
         puts "==== Failed to do voice! #{e.class}: #{e.message} ===="
         puts e.backtrace
@@ -49,6 +101,20 @@ bot.mention do |event|
       end
     else
       event.respond youre_not_in_a_voice_channel
+    end
+
+  when play_url?
+    if /(?<url>http\S+)/ =~ event.message.text
+      remember(user, :play_url, url)
+
+      if bot.voice.nil? || bot.instance_variable_get(:@voice_channel).id != user.voice_channel.try(:id)
+        event.respond im_not_in_a_voice_channel(user)
+        @conversation[user.id] = :asked_if_i_should_join_voice
+      else
+        Thread.new(&download_and_play_url(bot, event.channel.id, url))
+      end
+    else
+      event.respond missed_play(user)
     end
 
   else
